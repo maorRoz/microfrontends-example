a47535d1c35bba8af0cb430008e6b19ab2ed3186
maorRoz <andorys@gmail.com>
2021-12-19 01:42:14 +0200

    init2

---
+++ microfrontends-example/apps/app2/src/app/app.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <div
+        style={{ backgroundColor: 'blue', width: '500px', height: '500px' }}
+      ></div>
+    </StyledApp>
+  );
+}
+
+export default App;
---
+++ microfrontends-example/apps/main-app/src/app/app.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <App/>
+    </StyledApp>
+  );
+}
+
+export default App;
